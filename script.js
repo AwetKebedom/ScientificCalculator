20d794f2c3ef48374425f9e9c6b65ed3db81d387
Awet <awetkebedom@gmail.com>
2019-12-14 11:40:10 +0200

    added history function to the calculator

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function insert(num) {
     var a = document.form.textview;
 
     a.value += num;
+// <<<<<<< HEAD
    
+// =======
+    
+//     // histor += num;
+// >>>>>>> added history function to the calculator
     
 }
 function opt(amd) {
@@ -16,7 +21,11 @@ function opt(amd) {
     var view = document.form.textview;
     if (view.value != "") {
         view.value += amd;
+// <<<<<<< HEAD
         
+// =======
+//         // histor += amd;
+// >>>>>>> added history function to the calculator
         
     }
 }
@@ -26,16 +35,34 @@ function zero() {
         view.value += '0';
     }
 }
+var histor = [];
 function equal() {
     
+// <<<<<<< HEAD
    
+// =======
+//     // var button = document.getElementsByClassName('none');
+// >>>>>>> added history function to the calculator
     var a = document.form.textview;
     var exp = a.value;
     
 
     if (exp) {
-        try {a.value = math.evaluate(exp);}
+// <<<<<<< HEAD
+//         try {a.value = math.evaluate(exp);}
+//        catch (e) {
+// =======
+// <<<<<<< Updated upstream
+//         try {a.value = math.evaluate(exp);}
+//         catch (e) {
+// =======
+        try {a.value = a.value +'=' + math.evaluate(exp);
+        histor.push(a.value)
+        hist();
+        }
        catch (e) {
+// >>>>>>> Stashed changes
+// >>>>>>> added history function to the calculator
             alert ("Syntax Error!");
             document.form.textview.value = 'Syntax Error!';
             none();
@@ -62,6 +89,7 @@ function back() {
     var exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0, exp.length-1);
 }
+// <<<<<<< HEAD
 function sinFunc(){
     var a = document.form.textview.value;
     
@@ -73,3 +101,38 @@ function sqrFunc(){
     document.form.textview.value = Math.sqrt(a);
 }
 
+// =======
+// <<<<<<< Updated upstream
+// =======
+// function sinFunc(){
+//     var a = document.form.textview.value;
+    
+//     document.form.textview.value = Math.sin(a);
+// }
+// function sqrFunc(){
+//     var a = document.form.textview.value;
+    
+//     document.form.textview.value = Math.sqrt(a);
+// }
+function hist()
+{
+    var i;
+    for (i = 0; i < histor.length; i++) {
+        
+            // if (histor[i] == histor[(histor.length)-i] ){
+            //     continue;
+                
+            // }
+        
+        document.getElementById('result').innerHTML += histor[i] + '<br>';
+        document.getElementById('result').innerHTML += '   ';
+        histor = [];
+
+        
+      
+       
+    }
+    
+}
+// >>>>>>> Stashed changes
+// >>>>>>> added history function to the calculator
